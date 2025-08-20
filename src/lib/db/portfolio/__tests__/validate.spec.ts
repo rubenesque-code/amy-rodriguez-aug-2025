4792ce729725791fc0a48dae1a7e6ffd8e8375c6
rubenesque <a.ruben00001@gmail.com>
2025-08-20 17:30:35 +0100

    ...

--- src/lib/db/portfolio/__tests__/validate.spec.ts
+++ src/lib/db/portfolio/__tests__/validate.spec.ts
@@ -1,10 +1,14 @@
 import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { produce } from 'immer';
 
 import type { DbSchema } from '^db/~types';
 import { schema } from '../joi-schema';
-import { itemValid } from './mock-data';
-import { produce } from 'immer';
+import { imageComponentValid, itemValid } from './mock-data';
 import type { MakeOptional, MakeOptionalAtPath } from '^lib/types';
+
+// TODO
+// seperate out image component tests? make explicit referring to when only one image component
+// - descripe 'shallow fields'...
 
 function validate(item: DbSchema['Portfolio']) {
 	const { error, value } = schema.validate(item, {
@@ -28,12 +32,9 @@ describe('validate function', () => {
 	});
 
 	it('should validate a correct portfolio item', () => {
-		const item = itemValid;
-
-		const result = validate(item as DbSchema['Portfolio']);
+		const result = validate(itemValid);
 
 		expect(result.isValid).toBe(true);
-		expect(result.value).toStrictEqual(item);
 	});
 
 	it('should fail validation when id is missing', () => {
@@ -91,132 +92,37 @@ describe('validate function', () => {
 		expect(result.value.imageComponents[0].image.image.url).toBe(undefined);
 	});
 
-	/*
 	it('should fail validation when positions is empty', () => {
-		const item = {
-			id: 1,
-			order: 1,
-			imageComponents: [
-				{
-					id: 1,
-					layer: 1,
-					order: 1,
-					image: {
-						image: {
-							url: 'https://example.com/image.jpg'
-						}
-					},
-					positions: [], // Empty positions
-					widths: [{ id: 1, aspectRatio: 1.5, value: 300 }]
-				}
-			]
-		};
+		const item = produce(itemValid, (draft) => {
+			draft.imageComponents[0].positions = [];
+		}) as DbSchema['Portfolio'];
 
-		const result = validate(item as unknown as DbSchema['Portfolio']);
+		const result = validate(item);
+
 		expect(result.isValid).toBe(false);
 	});
 
-	it('should fail validation when positions has invalid data', () => {
-		const item = {
-			id: 1,
-			order: 1,
-			imageComponents: [
-				{
-					id: 1,
-					layer: 1,
-					order: 1,
-					image: {
-						image: {
-							url: 'https://example.com/image.jpg'
-						}
-					},
-					positions: [
-						{ id: 1, aspectRatio: 'invalid', x: 100, y: 200 } // Invalid aspectRatio
-					],
-					widths: [{ id: 1, aspectRatio: 1.5, value: 300 }]
-				}
-			]
-		};
-
-		const result = validate(item as unknown as DbSchema['Portfolio']);
-		expect(result.isValid).toBe(false);
-	});
-
-	// Invalid widths
 	it('should fail validation when widths is empty', () => {
-		const item = {
-			id: 1,
-			order: 1,
-			imageComponents: [
-				{
-					id: 1,
-					layer: 1,
-					order: 1,
-					image: {
-						image: {
-							url: 'https://example.com/image.jpg'
-						}
-					},
-					positions: [{ id: 1, aspectRatio: 1.5, x: 100, y: 200 }],
-					widths: [] // Empty widths
-				}
-			]
-		};
+		const item = produce(itemValid, (draft) => {
+			draft.imageComponents[0].widths = [];
+		}) as DbSchema['Portfolio'];
 
-		const result = validate(item as unknown as DbSchema['Portfolio']);
+		const result = validate(item);
+
 		expect(result.isValid).toBe(false);
 	});
 
-	it('should fail validation when widths has invalid data', () => {
-		const item = {
-			id: 1,
-			order: 1,
-			imageComponents: [
-				{
-					id: 1,
-					layer: 1,
-					order: 1,
-					image: {
-						image: {
-							url: 'https://example.com/image.jpg'
-						}
-					},
-					positions: [{ id: 1, aspectRatio: 1.5, x: 100, y: 200 }],
-					widths: [
-						{ id: 1, aspectRatio: 1.5, value: 'invalid' } // Invalid value
-					]
-				}
-			]
-		};
+	it('should pass validation when at least 1 valid image component and invalid image component(s)', () => {
+		const imageComponentInvalid = produce(imageComponentValid, (draft) => {
+			draft.id = 'abc' as unknown as number;
+		});
+		const item = produce(itemValid, (draft) => {
+			draft.imageComponents.push(imageComponentInvalid);
+		}) as DbSchema['Portfolio'];
 
-		const result = validate(item as unknown as DbSchema['Portfolio']);
-		expect(result.isValid).toBe(false);
-	});
+		const result = validate(item);
 
-	// Allow unknown fields
-	it('should allow unknown fields in the input', () => {
-		const item = {
-			id: 1,
-			order: 1,
-			unknownField: 'something', // Unknown field
-			imageComponents: [
-				{
-					id: 1,
-					layer: 1,
-					order: 1,
-					image: {
-						image: {
-							url: 'https://example.com/image.jpg'
-						}
-					},
-					positions: [{ id: 1, aspectRatio: 1.5, x: 100, y: 200 }],
-					widths: [{ id: 1, aspectRatio: 1.5, value: 300 }]
-				}
-			]
-		};
-
-		const result = validate(item as unknown as DbSchema['Portfolio']);
 		expect(result.isValid).toBe(true);
-		expect(result.value).toEqual(item);
-	}); */
+		expect(result.value.imageComponents).toStrictEqual([imageComponentValid]);
+	});
 });
